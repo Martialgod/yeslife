b0b023a9b1222d4ea4ca3d5e85c8b414e8af518f
Opic <floresopic@gmail.com>
2019-05-11 00:43:51 +0800

    update reward label
    * Can we make it say "REWARD DOLLARS:" rather than just "REWARDS:"

--- resources/views/landingpage/myaccount/totalrewards.blade.php
+++ resources/views/landingpage/myaccount/totalrewards.blade.php
@@ -1,4 +1,4 @@
-<h3>Rewards DOLLARS: ${{$totalpoints}} </h3>
+<h3>REWARD DOLLARS: ${{$totalpoints}} </h3>
 <span style="color:#af2424"> 
     Redeemable for online shop credit only. No cash value. 
 </span>
