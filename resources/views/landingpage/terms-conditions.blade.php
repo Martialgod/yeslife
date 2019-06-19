a237ef06fed2299af5e8a3f226cdf27bf8aa3980
Opic <floresopic@gmail.com>
2019-06-20 06:07:28 +0800

    harcoded privacy and terms condition page

--- resources/views/landingpage/terms-conditions.blade.php
+++ resources/views/landingpage/terms-conditions.blade.php
@@ -40,7 +40,90 @@
 
                         <div class="content"> 
 
-                            {!! $globalmessage->content !!}
+                            {{-- {!! $globalmessage->content !!} --}}
+
+                            <h3 class="title">TERMS OF USE</h3>
+
+                            <p>
+                            Welcome to the Yes.Life website (the "Website"), which is owned and operated by Yes.Life. Please review the following terms of use that govern your use of the Website (the "Terms"). Please note that your use of the Website constitutes your agreement to follow and be bound by these Terms. If you do not agree to these Terms, please leave this Website immediately. Yes.Life may revise these Terms from time to time, without prior notice, so you should visit this page periodically to review them.
+                            </p>
+
+                            <h3 class="title">CUSTOMER RATINGS &amp; REVIEWS AND STORIES</h3>
+
+                            <p>
+                            The Terms govern the Testimonial submissions offered for use by Yes.Life. To the extent there is any conflict between Yes.Life's Privacy Policy and these Terms of Use, these Terms of Use shall control with respect to the Testimonials. <br>
+                            By submitting any content to Yes.Life, you represent and warrant that: you are the sole author and owner of all intellectual property rights thereto; you waive all "moral rights" that you may have in such content; all content that you post is accurate; your content does not violate these Terms of Use; your content will not cause injury to any other party; your content is not false, inaccurate or misleading; your content does not infringe any third party's copyright, patent, trademark, trade secret or other proprietary rights or rights of publicity or privacy; your content does not violate any law, statute, ordinance or regulation (including, but not limited to, those governing export control, consumer protection, unfair competition, anti-discrimination or false advertising); your content will not reasonably be considered to be, defamatory, libelous, hateful, racially or religiously biased or offensive, unlawfully threatening or unlawfully harassing to any individual, partnership or corporation; you were not compensated or granted any consideration by any third party for submitting your content; your content does not include any information that references other websites, addresses, email addresses, contact information or phone numbers; your content is not subject to any confidentiality obligations, nor will Yes.Life be subject to any confidentiality obligations on account of your content; your content does not contain any computer viruses, worms or other potentially damaging computer programs or files. <br>
+
+
+                            You agree to indemnify and hold Yes.Life (and its officers, directors, agents, subsidiaries, joint ventures, employees and third-party service providers), harmless from all claims, demands, and damages (actual and consequential) of every kind and nature, known and unknown including reasonable attorneys' fees, arising out of a breach of your representations and warranties set forth above, your use of this Website, or your violation of any law or the rights of a third party. <br>
+                            For any content that you submit, you grant to Yes.Life a perpetual, irrevocable, royalty-free, transferable right and license to use, copy, modify, delete in its entirety, adapt, publish, translate, create derivative works from, sell or distribute such content and to incorporate such content into any form, medium or technology throughout the world without compensation to you.
+
+                            <span id="return-policy"> 
+                            All content that you submit may be reviewed, revised, posted, or otherwise used by Yes.Life at Yes.Life's sole discretion. Yes.Life reserves the right to change, condense or delete any content on Yes.Life's website. Yes.Life does not guarantee that you will have any recourse through Yes.Life to edit or delete any content you have submitted. Yes.Life reserves the right to remove or to refuse to post any submission for any reason. You acknowledge that you, not Yes.Life, are responsible for the contents of your submission. <br>
+
+                            By submitting your email address in connection with the submission, you agree that Yes.Life and its third-party service providers may use your email address to contact you about the status of your submission and other administrative purposes.  </span>
+                            </p>
+
+
+                            <h3 class="title">RETURN AND REFUND POLICY</h3>
+
+
+                            <p>
+                            Yes.Life keeps its promise to provide natural products to help you be on the right track to healthier and happier life. We can prove this with our return policy.  If you’re not satisfied with your purchase from Yes.Life, just contact us within 30 days and we will you a refund just like that.  No fuss! <br>
+
+                            We hope that you won’t ever have to consider returning our products, but if unavoidable, we will make the process as worry-free as possible.  We pride ourselves with our 30-day Satisfaction Guarantee.  Please remember that we are also covered by a <a href="#limitations-of-liability"> <strong> Limited Warranty. </strong> </a>
+                            Only purchases made directly through <a href="/"> Yes.Life </a> are covered by the 30-day Return Policy.  For all other Yes.Life purchases, contact the authorized distributor near you for merchant guarantees.
+                            </p>
+
+
+                            <h3 class="title">WEBSITE CONTENT AND ORDERS</h3>
+
+                            <p>
+                            Please understand that we have made every effort to display the products featured on our Website as accurately as possible. However, the colors we use, as well as the display and color capabilities of your particular computer monitor, will affect the colors you see on the screen. Yes.Life is not responsible for the limitations of your monitor and cannot guarantee that your monitor's display of any color, texture, or detail of merchandise will be accurate. If you are unhappy with your purchase for any reason, you may simply return the item. Click here for our <a href="#return-policy"> Return Policy </a> <br>
+
+                            We reserve the right to correct any errors, inaccuracies or omissions and to change or update information at any time without prior notice (including after you have submitted your order). We reserve the right at any time after receipt of your order to accept or decline your order, or any portion thereof, even after your receipt of order confirmation. For example, in the event that a product is listed at an incorrect price, we shall have the right to refuse or cancel orders placed for the product listed at the incorrect price, regardless of whether the order has been confirmed and/or your credit card charged. If your credit card has already been charged for the purchase and your order is canceled, we will promptly issue a credit to your credit card account in the amount of the incorrect price. We apologize for any inconvenience this may cause you. <br>
+
+                            We reserve the right, at our sole discretion, to limit the number of items purchased and to prohibit sales to dealers. The prices displayed on this Website are quoted in U.S. currency and as such are valid only in the United States. Prices are subject to change at any time.
+                            </p>
+
+
+                            <h3 class="title">INTELLECTUAL PROPRIETARY RIGHTS</h3>
+
+                            <p>
+                            This Website is expressly owned and operated by Yes.Life. Unless otherwise noted, all content on this Website, such as text, graphics, logos, trademarks, service marks and trade names, button icons, images, audio clips, digital downloads, data compilations, and software, is the exclusive property of Yes.Life. This Website, as a whole, is protected by copyright, trademark, trade dress, intellectual property, and other applicable laws. All worldwide rights are reserved.
+                            </p>
+
+                            <h3 class="title" id="limitations-of-liability">LINKS TO OTHER WEBSITES AND SERVICES</h3>
+
+                            <p>
+                            This Website may contain links to other websites that are not under the control of Yes.Life. Yes.Life has no responsibility for the linked websites nor does it necessarily endorse them. Yes.Life provides the links solely for the convenience and information of its Website users.
+                            </p>
+
+
+                              
+                            <h3 class="title">DISCLAIMER: LIMITATIONS OF LIABILITY</h3>
+
+                            <p>
+                            Yes.Life provides the materials on this Website "as is" without warranties of any kind, either express or implied, including without limitation, warranties of title, implied warranties of merchantability, fitness for a particular purpose or non-infringement of intellectual property. Yes.Life expressly disclaims any duty to update or revise the materials on this Website. Yes.Life may modify the materials at any time without notice. By your use of this Website, you assume all risk for your use of the Website and that you assume full responsibility for all costs associated with all necessary servicing or repairs of any equipment you use in connection with your use of this Website.
+                            </p>
+
+
+                            <p>
+                            IN NO EVENT SHALL Yes.Life OR ANY OF ITS AFFILIATED ENTITIES OR SUPPLIERS BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES, WHETHER IN AN ACTION, UNDER CONTRACT, NEGLIGENCE OR ANY OTHER THEORY, ARISING OUT OF OR IN CONNECTION WITH THE USE, INABILITY TO USE OR PERFORMANCE OF THE INFORMATION, SERVICES, PRODUCTS AND MATERIALS AVAILABLE FROM THE WEBSITE, EVEN IF Yes.Life HAS BEEN PREVIOUSLY ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+                            These limitations shall apply notwithstanding any failure of essential purpose or the existence of any limited remedy. Because some jurisdictions do not allow limitations on how long an implied warranty lasts or the exclusion or limitation of liability for consequential or incidental damages, the above limitations may not apply to you.
+                            </p>
+
+                            <h3 class="title">CONFIDENTIALITY OF PASSWORDS AND OTHER ACCOUNT IDENTIFIERS</h3>
+
+                            <p>
+                            If you create an account on the Website you are responsible for: (i) maintaining the confidentiality of passwords and other account identifiers you choose, and (ii) all activities that occur under or in connection with such passwords and account identifiers. You agree to notify Yes.Life immediately in the event of: (i) any loss of your password or other account identifiers or (ii) any unauthorized use of your password or other account identifiers. Yes.Life will not be responsible or liable, directly or indirectly, in any way for any loss or damage of any kind incurred as a result of, or in connection with, your failure to comply with these Terms.
+                            </p>
+
+                            <h3 class="title">USER COMMENTS AND SUBMISSIONS</h3>
+
+                            <p>
+                            We welcome your comments and input about our Website and products. However, any comments, ideas, questions, feedback, notes, messages, suggestions or other communications (collectively "Comments") sent to our Website shall be and remain the exclusive property of Yes.Life. If you submit any Comments to us, such submission shall constitute an assignment to Yes.Life of all worldwide rights, titles, and interests in all copyrights, patent rights, and other intellectual property rights in the Comments. Yes.Life will be entitled to use, modify, reproduce, disclose, publish and distribute any material you submit for any purpose whatsoever, without restriction and without compensating you in any way. For this reason, you should not send us any Comments that you do not wish to assign to us, including without limitation any confidential information or any original creative materials such as product ideas, designs, inventions, computer code, stories or original artwork.
+                            </p>
 
                         </div><!--END content-->
 
